249f9e6910af374af3912d6f6453252210bd4ace
MFloresr <mrsitofull@gmail.com>
2014-11-05 18:18:53 +0100

    gg

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-
+var milista = new List();
 var divContainer = document.getElementById('container');
 var divResultado = document.getElementById('resultado');
 
 var TextoDeArea = document.createElement('textarea');
     TextoDeArea.type ='textarea';
+   // TextoDeArea.idName = 'texto';
+    TextoDeArea.setAttribute('id','texto')
     divContainer.appendChild(TextoDeArea);
 
 var BotonAdd = document.createElement('input');
@@ -16,13 +18,28 @@ var BotonClear = document.createElement('input');
     BotonClear.value = 'Limpiar';
     divContainer.appendChild(BotonClear);
 
-var milista = new list();
-
-list.prototype.AddTable = function () {
-
+List.prototype.addTable = function () {
+    var tabla = document.createElement('table');
+    divResultado.appendChild(tabla);
+    for (var i= 0; i < milista.listsize; i++) {
+        
+    
+    var tr = document.createElement('tr')
+    var tr = document.createElement('td')
+    divResultado.tabla.appendChild(tr);
+    divResultado.tabla.appendChild(td);
+    }
 }
 
-list.prototype.RemoveId = function (id) {
+List.prototype.removeId = function (id) {
     this.dataStore.splice(id, 1);
     --this.pos;
 }
+
+BotonAdd.addEventListener('click', capturarDatos);
+
+function capturarDatos() {
+    var miTexto = document.getElementById('texto').value;
+    milista.append(miTexto);
+}
+
